2b91a77e6efbda2c9d4a4a6a39977727ca904ca1
taichikid <taichi1021kid@gmail.com>
2021-01-11 10:23:18 +0900

    first-commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <input type="text" maxlength="7">
+    <button @click="$router.push({ name: 'Home' })">戻る</button>
+    <p>Adress:{{ adressData }}</p>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  props: ["adress"],
+  data() {
+    return {
+      adressData: "",
+    };
+  },
+  async create() {
+    let item = await axios.get(
+      `https://apis.postcode-jp.com/api/v4/postcodes/${this.adress}?appid=Dd1J4ob9W0NAuKy8W3AqhMMEVvaDCJFFFws7Lkn
+`
+    );
+    this.adressData = item.data.allAdress;
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" maxlength="7" v-model="adress">
+    <button @click="$router.push({ name: 'About', params: { adress: adress} })">住所自動入力</button>
+    <p>Adress:</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      adrees: ""
+    };
   }
-}
+};
 </script>
